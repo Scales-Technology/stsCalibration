9832a719134f0a88e82aa85f27127e2f7b6c7989
Nkunja <munene01denis@gmail.com>
2024-02-17 10:59:00 +0300

    new client registration

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom';
 import HomePage from './Screen/Homepage/Homepage';
 import Login from './Screen/Pages/Login';
+import NewClient from './Screen/Pages/Newclient';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={< HomePage />} />
         <Route path="/login" element={< Login />} />
+        <Route path="/new-client" element={< NewClient />} />
         <Route path="*" element={< Navigate to=" /" />} />
       </Routes>
     </Router>
